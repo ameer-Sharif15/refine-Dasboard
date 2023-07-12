eb1e375045b5d74641e41300cbde4c1e3e707bfb
hafeez abubakar <89017864+ameer-Sharif15@users.noreply.github.com>
2023-07-12 21:17:15 +0100

    initialization

---
+++ src/components/home/TopAgent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopAgent = () => {
+  return (
+    <div>TopAgents</div>
+  )
+}
+
+export default TopAgent
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,25 @@
-export { Header } from "./header";
+import Profile from './common/Profile';
+import PropertyCard from './common/PropertyCard';
+import CustomButton from './common/CustomButton';
+
+// charts
+import PieChart from './charts/PieCharts';
+import PropertyReferrals from './charts/PropertyReferrals';
+import TotalRevenue from './charts/TotalRevenue';
+
+// agent
+import AgentCard from './agent/AgentCard';
+
+// home
+import TopAgent from './home/TopAgent';
+
+export {
+  Profile,
+  PropertyCard,
+  CustomButton,
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  AgentCard,
+  TopAgent,
+};
